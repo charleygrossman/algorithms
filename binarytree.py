0680b0328f9a65c4ebda575dc20b33d33de6b9d4
chuck <chuck@chucks-MacBook-Pro.local>
2017-09-17 00:48:11 -0500

    WIP: binarytree

---
+++ binarytree.py
@@ -0,0 +1,75 @@
+def main():
+    print("Enter [q] to quit")
+    print("Welcome to the binary tree interface! Here are the methods: " +
+          "insert, size")
+
+    binaryTree = BinaryTree()
+
+    while True:
+        i = input("Enter a command (insert, size): " )
+        if i == "q": break
+
+        if i == "insert":
+            e = input("Name the element you want to insert: ")
+            binaryTree.insert(e)
+
+        if i == size():
+            binaryTree.size()
+
+class Node(object):
+
+    def __init__(self, value=None, parent=None):
+        self.value = value
+        self.parent = parent
+        self.left = None
+        self.right = None
+
+    def get_value(self):
+        return self.value
+
+    def get_parent(self):
+        return self.parent
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.right
+
+class BinaryTree(object):
+
+    def __init__(self):
+        self.root = Node(None)
+        self.parent = None
+        self.left = None
+        self.right = None
+
+    def insert(self, child, parent=self.root):
+        if parent.get_value() is None:
+            new_node = Node(child)
+            parent = new_node
+        else:
+            if e < parent.get_value():
+                insert(self, child, parent.get_left())
+            if e > parent.get_value():
+                insert(self, child, parent.get_right())
+
+    # REFACTOR: Use BFS or DFS
+    def size(self):
+        count = 0
+        current = self.root
+        # Count the left
+        while current.get_value() is not None:
+            count += 1
+            current = current.get_left()
+        # Count the right
+        current = self.root
+        count -= 1
+        while current.get_value() is not None:
+            count += 1
+            current = current.get_right()
+
+        print(count)
+
+if __name__ == "__main__":
+    main()
--- graph.py
+++ graph.py
@@ -1,20 +1,21 @@
-# NOTE: WIP (Currently just an adjaceny list maker)
-
 def main():
     print("Enter [q] to quit")
+    print("This uses an adjaceny-list representation of a graph")
 
     while True:
-        sets = input("Enter a list of vertices separated by space to " +
-        "denote edges from the first in the list to the rest, and separate " +
-        "multiple lists by commas: ").split(",")
+        sets = input("Build your graph by entering a list of vertices separated" +
+        " by space to denote edges from the first in the list to the rest," +
+        " and separate multiple lists by commas: ").split(",")
         if sets[0] == "q": break
 
-        adjList = [0] * len(sets)
+        # Build the graph G as an adjaceny-list
+        G = [0] * len(sets)
         for s in sets:
             tmp = s.split()
             tmp = [int(x) for x in tmp]
-            adjList[tmp[0]] = tmp[1:]
+            G[tmp[0]] = tmp[1:]
 
+        
 
 
 if __name__ == "__main__":
