490abfad2fdf37bc72a774c90addc64760b2d5a3
chuck <chuck@chucks-MacBook-Pro.local>
2018-02-17 15:34:56 -0600

    quicksort_2.py - Sort by choice of pivot

--- quicksort.py
+++ quicksort.py
@@ -1,17 +1,19 @@
 cnt = 0
 
 def main():
-    with open('quicksort_test.txt') as f:
-        A = []
-        for line in f:
-            A.append([int(x) for x in line.split()])
-        quicksort(A, 0, len(A)-1)
-        print(A)
-        print(cnt)
+    # with open('quicksort_test.txt') as f:
+    #     A = []
+    #     for line in f:
+    #         A.append([int(x) for x in line.split()])
+    #     quicksort(A, 0, len(A)-1)
+    #     print(A)
+    #     print(cnt)
+    arr = [1, 6, 2, 4, 3, 0, 9, 7, 2, 8]
+    quicksort(arr, 0, len(arr)-1)
+    print(arr)
 
 def quicksort(arr, lo, hi):
     if lo >= hi: return
-
     p = partition(arr, lo, hi)
     quicksort(arr, lo, p-1)
     quicksort(arr, p+1, hi)
@@ -20,21 +22,26 @@ def quicksort(arr, lo, hi):
 # j := separates seen from unseen
 # pivot is arr[hi]
 def partition(arr, lo, hi):
-    pivot = arr[hi]
-    i = lo-1
-
-    for j in range(lo, hi):
+    # pivot = arr[hi]
+    # i = lo-1
+    # for j in range(lo, hi):
+    #     global cnt
+    #     cnt += 1
+    #     if arr[j] <= pivot:
+    #         i += 1
+    #         arr[i], arr[j] = arr[j], arr[i]
+    # arr[i+1], arr[hi] = arr[hi], arr[i+1]
+    # return i + 1
+    pivot = arr[lo]
+    i = lo+1
+    for j in range(lo+1, hi):
         global cnt
         cnt += 1
-        if arr[j] <= pivot:
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
             i += 1
-            tmp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = tmp
-    tmp = arr[i+1]
-    arr[i+1] = arr[hi]
-    arr[hi] = tmp
-    return i + 1
+    arr[i-1], arr[lo] = arr[lo], arr[i-1]
+    return i-1
 
 # def choose_pivot(A, mode):
 #     if mode == "first": return A[0]
--- quicksort_2.py
+++ quicksort_2.py
@@ -1,40 +1,44 @@
-# cnt = 0
+cnt = 0
 
 def main():
-    print("Enter [q] to quit")
-    A = [1, 6, 2, 4, 3, 0, 9, 7, 2, 8]
-    # with open('quicksort_test.txt') as f:
-    #     A = []
-    #     for line in f:
-    #         A.append([int(x) for x in line.split()])
-    quicksort(A, "first")
+    with open('quicksort_test.txt') as f:
+        A = []
+        for line in f:
+            A.append([int(x) for x in line.split()])
+    quicksort(A, 0, len(A)-1)
     print(A)
-        # global cnt
-        # print(cnt)
-        # cnt = 0
+    global cnt
+    print(cnt)
+    cnt = 0
 
-def quicksort(A, mode):
-    if len(A) <= 1: return
-    p = choose_pivot(A, mode)
-    partition(A, p)
-    quicksort(A[:A.index(p)], mode)
-    quicksort(A[A.index(p)+1:], mode)
+def quicksort(A, l, r):
+    if l >= r: return
+    p = partition(A, l, r)
+    quicksort(A, l, p-1)
+    quicksort(A, p+1, r)
 
-def partition(A, p):
-    hi = len(A)
-    i = 1
-    for j in range(1, hi):
-        # global cnt
-        # cnt += 1
+# TODO all start at l+1
+def partition(A, l, r):
+    tmp = choose_pivot(A, l, r, "median")
+    A[l], A[tmp] = A[tmp], A[l]
+    p = A[l]
+    i = l+1
+    global cnt
+    cnt += r-l
+    for j in range(l+1, r+1):
         if A[j] < p:
             A[j], A[i] = A[i], A[j]
             i += 1
-    A[A.index(p)], A[i-1] = A[i-1], A[A.index(p)]
+    A[l], A[i-1] = A[i-1], A[l]
+    return i-1
 
-def choose_pivot(A, mode):
-    if mode == "first": return A[0]
-    elif mode == "last": return A[len(A) - 1]
-    elif mode == "median": return sorted(A[0], A[len(A)//2], A[len(A)-1])[1]
+
+def choose_pivot(A, l, r, mode):
+    if mode == "first": return l
+    elif mode == "last": return r
+    elif mode == "median":
+        tmp = sorted([A[l], A[l + ((r-l)//2)], A[r]])
+        return A.index(tmp[1])
     else: raise ValueError("Wrong mode")
 
 if __name__ == "__main__": main()
