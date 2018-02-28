e49ee8adec807f9fde40656b9bdc61823b8a3230
chuck <chuck@chucks-MacBook-Pro.local>
2018-02-27 22:37:41 -0600

    min_cut.py needs more efficient random edge chooser

--- min_cut.py
+++ min_cut.py
@@ -1,54 +1,66 @@
 import re
 import random as rand
 
+
 best_cut = 0
 
 def main():
     print("Compute the minimum cut of a given graph")
-    G = {}
+    G_prime = {}
     with open('min_cut_test.txt') as f:
         for line in f:
             line = line.rstrip()
             tmp = re.split(r"[\D']+", line)
             row = [int(x) for x in tmp]
-            G[row[0]] = row[1:]
-    test_min_cut(G, 250)
+            G_prime[row[0]] = row[1:]
+    for i in range(1):
+        print("In test #{}".format(i))
+        G = G_prime.copy()
+        test_min_cut(G)
     global best_cut
     print(best_cut)
 
-def test_min_cut(G, N):
+def test_min_cut(G):
     global best_cut
-    for i in range(N):
-        new_cut = compute_min_cut(G)
-        if new_cut < best_cut:
-            best_cut = new_cut
+    new_cut = compute_min_cut(G)
+    print(new_cut)
+    if new_cut < best_cut: best_cut = new_cut
 
 def compute_min_cut(G):
     while len(G) > 2:
-        # Choose edge unif at random
         e = choose_edge(G)
-        # Contract e.u, e.v into a single vertex, and remove self-loops
         contract(G, e)
+    return min_cut_size(G)
 
-    # return cut represented by final 2 (number of edges between them)
-
+# TODO: Fails more as graph size decreases
 def choose_edge(G):
+    print("Graph size: {}".format(len(G)))
     while True:
         u = rand.randint(1, 200)
         try:
             v = rand.randrange(len(G[u]))
             return (u, G[u][v])
-        except:
-            KeyError
+        except: continue
 
 def contract(G, e):
     v, w = e
     G[v].remove(w)
-    G[w].remove(v)
-    
-    # Remove self-loops
+    if w in G:
+        if v in G[w]: G[w].remove(v)
+        G[v] += G[w]
+        for val in G[w]:
+            if val in G: G[val].append(v)
+        if v != w: del G[w]
     if v in G[v]: G[v].remove(v)
-    if w in G[w]: G[w].remove(w)
+
+def min_cut_size(G):
+    count = 0
+    G_iter = G.iterkeys()
+    node_a = G_iter.next()
+    node_b = G_iter.next()
+    for e in G[node_a]:
+        if e == node_b: count += 1
+    return count
 
 
 if __name__ == "__main__": main()
