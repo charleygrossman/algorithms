9ab3c438a70d6e45ec149605a91454d785b20329
chuck <chuck@chucks-MacBook-Pro.local>
2018-10-27 11:36:26 -0400

    peek() method added to queue_from_stacks.py

--- queue_from_stacks.py
+++ queue_from_stacks.py
@@ -2,7 +2,7 @@ def main():
     print("Enter [q] to quit")
     print("Implement a queue using two stacks")
     while True:
-        i = input("Pop and push from your queue by typing pop or push value: ").split(" ")
+        i = input("Pop and push from your queue by typing pop or push value (can also peek): ").split(" ")
         q = my_queue()
         if i[0] == "q":
             break
@@ -10,26 +10,36 @@ def main():
             print(q.pop())
         elif i[0] == "push":
             q.push(i[1])
+        elif i[0] == "peek":
+            print(q.peek())
         else:
             raise ValueError("Wrong input")
 
 
 class my_queue(object):
+    
     def __init__(self):
-        self.stack_oldest = []
-        self.stack_newest = []
+        self.stk_old = []
+        self.stk_new = []
 
     def push(self, val):
-        self.stack_newest.append(val)
+        self.stk_new.append(val)
 
     def pop(self):
         self.shift_stacks()
-        return self.stack_oldest.pop()
+        return self.stk_old.pop()
+
+    def peek(self):
+        # stk_old may not always be nonempty, but when it is, it's guaranteed to have the next-up
+        if self.stk_old:
+            return self.stk_old[-1]
+        else:
+            return self.stk_new[0]
 
     def shift_stacks(self):
-        if not self.stack_oldest:
-            while self.stack_newest:
-                self.stack_oldest.append(self.stack_newest.pop())
+        if not self.stk_old:
+            while self.stk_new:
+                self.stk_old.append(self.stk_new.pop())
 
 
 if __name__ == "__main__":
